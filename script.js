d72f08823d265cedeb251c863224f517e46ba020
aisswaryya <murugananth.a@husky.neu.edu>
2020-12-29 21:41:55 -0800

    fixed transition funtion

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ const HOURHAND = document.querySelector('#hour');
 const MINUTEHAND = document.querySelector('#minute');
 const SECONDHAND = document.querySelector('#second');
 
-function clock() {
-    var date = new Date();
-    console.log(date);
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    console.log('Hour: ' + hr + ' Minute: ' + min + ' Second: ' + sec);
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log('Hour: ' + hr + ' Minute: ' + min + ' Second: ' + sec);
 
-    let hrPosition = (hr * 360) / 12 + (min * (360 / 60)) / 12;
-    let minPosition = (min * 360) / 60 + (sec * (360 / 60)) / 60;
-    let secPosition = (sec * 360) / 60;
+let hrPosition = (hr * 360) / 12 + (min * (360 / 60)) / 12;
+let minPosition = (min * 360) / 60 + (sec * (360 / 60)) / 60;
+let secPosition = (sec * 360) / 60;
+
+function clock() {
+    hrPosition = hrPosition + 3 / 360;
+    minPosition = minPosition + 6 / 60; //(1/60)*6
+    secPosition = secPosition + 6; //360/60
 
     HOURHAND.style.transform = 'rotate(' + hrPosition + 'deg)';
     MINUTEHAND.style.transform = 'rotate(' + minPosition + 'deg)';
